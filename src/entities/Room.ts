487025d102bf025efd349ad7ce1c4261fb211368
Wellington Silva <wsfin64@gmail.com>
2024-07-01 21:23:22 -0300

    criando relação n-m

--- src/entities/Room.ts
+++ src/entities/Room.ts
@@ -1,5 +1,6 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToMany, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Video } from "./Video";
+import { Subject } from "./Subject";
 
 @Entity('rooms')
 export class Room {
@@ -15,4 +16,7 @@ export class Room {
 
     @OneToMany(() => Video, video => video.room)
     videos: Video[];
+
+    @ManyToMany(() => Subject, subject => subject.rooms)
+    subjects: Subject[];
 }
